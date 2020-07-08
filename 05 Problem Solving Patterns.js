83f486a9b6541a9e6f00172995f0577a93d8d916
eslessons <66363651+eslessons@users.noreply.github.com>
2020-07-08 19:31:52 +0300

    05 Problem Solving Patterns is in progress. Function countUniqueValues is ready

--- 05 Problem Solving Patterns.js
+++ 05 Problem Solving Patterns.js
@@ -107,9 +107,7 @@
   //   if (first.length !== second.length) {
   //     return false;
   //   }
-
   //   const lookup = {};
-
   //   for (let i = 0; i < first.length; i++) {
   //     let letter = first[i];
   //     // if letter exists, increment, otherwise set to 1
@@ -125,10 +123,8 @@
   //       lookup[letter] -= 1;
   //     }
   //   }
-
   //   return true;
   // }
-
   // console.log(validAnagram('', '')); // true
   // console.log(validAnagram('aaz', 'zza')); // false
   // console.log(validAnagram('anagram', 'nagaram')); // true
@@ -138,22 +134,126 @@
   // console.log(validAnagram('texttwisttime', 'timetwisttext')); // true
 })();
 
-/* ~~~~~~~~~~~~~ Anagram Challenge Solution ~~~~~~~~~~~~~ */
+/* ~~~~~~~~~~~~~ Multiple Pointers Pattern ~~~~~~~~~~~~~ */
+
+// Creating pointers or values that correspond to an
+//  index or position and move towards the beginning
+//  end or middle based on a certain condition.
+
+// Very efficient for solving problems with minimal
+//  space complexity as well.
+
+// [-4, -3, -2, -1, 0, 1, 2, 5]
+// "alksjdalksjdlkasjdlks"
+// ...LinkedLists...
+//
+
+// An Example
+
+// Write a function called sumZero which accepts
+//  a sorted array of integers.
+// The function should find the first pair where
+//  the sum is 0.
+// Return an array that includes both values that
+//  sum to zero or undefined if a pair does not
+//  exist.
+
+// sumZero([-3,-2,-1,0,1,2,3]) // [-3,3]
+// sumZero([-2,0,1,3]) // undefined
+// sumZero([1,2,3]) // undefined
 
 (() => {
-  //
+  // // Naive Solution
+  // // Time Complexity - O(N^2)
+  // // Space Complexity - O(1)
+  // function sumZero(arr) {
+  //   for (let i = 0; i < arr.length; i++) {
+  //     for (let j = i + 1; j < arr.length; j++) {
+  //       if (arr(i) + arr[j] === 0) {
+  //         return [arr[i], [arr[j]]];
+  //       }
+  //     }
+  //   }
+  // }
+  // sumZero([-3,-2,-1,0,1,2,3]) // [-3,3]
+  // sumZero([-2,0,1,3]) // undefined
+  // sumZero([1,2,3]) // undefined
 })();
 
 (() => {
-  //
+  // // Mutiple Pointers
+  // // My attempt
+  // function sumZero(arr) {
+  //   result = [];
+  //   for (let i = 0; i < arr.length / 2; i++) {
+  //     let j = arr.length - 1;
+  //     if (arr[i] + arr[j] === 0) {
+  //       result.push(arr[i]);
+  //       result.push(arr[j]);
+  //       return result;
+  //     } else if (arr[i] + arr[j] > 0) {
+  //       j--;
+  //     } else if (arr[i] + arr[j] < 0) {
+  //       i++;
+  //     }
+  //   }
+  // }
+  // console.log(sumZero([-3, -2, -1, 0, 1, 2, 3])); // [-3,3]
+  // console.log(sumZero([-2, 0, 1, 3])); // undefined
+  // console.log(sumZero([1, 2, 3])); // undefined
 })();
 
 (() => {
-  //
+  // // Multiple Pointers Solution
+  // // Refactor
+  // // Time Complexity - O(N)
+  // // Space Complexity - O(1)
+  // function sumZero(arr) {
+  //   let left = 0;
+  //   let right = arr.length - 1;
+  //   while (left < right) {
+  //     let sum = arr[left] + arr[right];
+  //     if (sum === 0) {
+  //       return [arr[left], arr[right]];
+  //     } else if (sum > 0) {
+  //       right--;
+  //     } else {
+  //       left++;
+  //     }
+  //   }
+  // }
+  // console.log(sumZero([-3, -2, -1, 0, 1, 2, 3])); // [-3,3]
+  // console.log(sumZero([-2, 0, 1, 3])); // undefined
+  // console.log(sumZero([1, 2, 3])); // undefined
 })();
 
+/*Multiple Pointers Pattern Count Unique Values Challenge*/
+
+// Implement a function called countUniqueValues,
+//  which accepts asorted array, and counts the
+//  unique values in the array. There can be
+//  negative numbers in the array, but it will
+//  always be sorted.
+
+// countUniqueValues([1,1,1,1,1,2]) // 2
+// countUniqueValues([1,2,3,4,4,4,7,7,12,12,13]) // 7
+// countUniqueValues([]) // 0
+// countUniqueValues([-2,-1,-1,0,1]) // 4
+
 (() => {
-  //
+  function countUniqueValues(arr) {
+    let i = 0;
+
+    for (let j = 1; j < arr.length; j++) {
+      if (arr[i] !== arr[j]) {
+        i++;
+        arr[i] = arr[j];
+      }
+    }
+    // arr.length = i + 1;
+    // return arr;
+    return i + 1;
+  }
 })();
 
 (() => {
