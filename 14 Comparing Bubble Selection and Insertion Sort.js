36a1c1dce92dc624d66edf8b54aae48851f18ba4
eslessons <66363651+eslessons@users.noreply.github.com>
2020-07-10 10:28:19 +0300

    14 Comparing Bubble Selection and Insertion Sort is finished

---
+++ 14 Comparing Bubble Selection and Insertion Sort.js
@@ -0,0 +1,18 @@
+/*      Comparing Bubble Selection and Insertion Sort      */
+
+// Big O of Sorting Algorithms
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *           *   Time   *   Time    *   Time   *   Space    *
+ * Algorithm * Complex. * Complex.  * Complex. * Complexity *
+ *           *  (Best)  * (Average) * (Worst)  *            *
+ ** * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *  Bubble   *   O(N)   *   O(N^2)  *  O(N^2)  *     O(1)   *
+ *   sort    *          *           *          *            *
+ ** * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Insertion *   O(N)   *   O(N^2)  *  O(N^2)  *     O(1)   *
+ *   sort    *          *           *          *            *
+ ** * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Selection *  O(N^2)  *   O(N^2)  *  O(N^2)  *     O(1)   *
+ *   sort    *          *           *          *            *
+ ** * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
